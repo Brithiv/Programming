cfec21aa4d3ef8f3568ee9199e711f87f1a778e0
Brithiv <bkbrithivin@gmail.com>
2024-08-04 15:58:37 +0530

    Program

--- Even Number.py
+++ Even Number.py
@@ -1,3 +1,9 @@
+# Using for loop:
+# Even Numbers from 2-20:
+
+for i in range(2,21,2):
+    print(i, end=" ")
+
 # Other Method:
 # -------------
 
@@ -5,3 +11,12 @@ count = 2
 while count<=20 :
     print(count, end=" ")
     count+=2
+
+# Using while loop:
+# -----------------
+start = 2
+end =20
+while start <= end :
+    if start % 2 == 0 :
+        print(start, end =" ")
+    start += 1
