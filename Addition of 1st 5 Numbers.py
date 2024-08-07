92e272a27fe8da5e15791ac35bb311df50548321
Biruthivin B K <131096179+Brithiv@users.noreply.github.com>
2024-08-07 08:34:35 +0530

    Programs

--- Addition of 1st 5 Numbers.py
+++ Addition of 1st 5 Numbers.py
@@ -8,5 +8,17 @@ for x in range(1, num + 1):
     total = total + x
 print(total)
 
+# Using while loop
+# ----------------
+
+initial_amount = 0
+amount = 1
+days = 10
+
+while amount<=days:
+	initial_amount = initial_amount + amount
+	amount += 1
+print(initial_amount)
+
 
 
