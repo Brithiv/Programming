04ed29536968f177cc48c648c48c3a3ce28e736b
Biruthivin B K <131096179+Brithiv@users.noreply.github.com>
2024-08-12 18:43:22 +0530

    Find Vowels.py

--- Find Vowels.py
+++ Find Vowels.py
@@ -7,3 +7,15 @@ while i < length :
 	if name[i] in ['a', 'e', 'i', 'o', 'u'] :
 		print(name[i], end=' ')
 	i += 1
+
+	(OR)
+
+name = input("Enter your name: ")
+
+i = 0
+length = len(name)
+
+while i < length :
+	if name[i]=='a' or name[i]=='e' or name[i]=='i' or name[i]=='o' or name[i]=='u' :
+		print(name[i])
+	i += 1
