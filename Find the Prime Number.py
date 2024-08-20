7c630751226578a9f6c1503ea0d17c569caee8a1
Biruthivin B K <131096179+Brithiv@users.noreply.github.com>
2024-08-20 07:08:29 +0530

    To Find the Prime Number.py

--- Find the Prime Number.py
+++ Find the Prime Number.py
@@ -4,7 +4,7 @@ div = 2
 while div < num :
     if num % div == 0 :
         print("Not a Prime Number")
-        break			# give a non-prime number then only you will know why you are using 'break' here.
+        break			
     div += 1
 else:
     print("It is a Prime Number")
