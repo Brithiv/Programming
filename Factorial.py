8c6249e834d736c3a8d8c6627df78328b16dc51b
Biruthivin B K <131096179+Brithiv@users.noreply.github.com>
2024-09-07 08:07:10 +0530

    Programs

--- Factorial.py
+++ Factorial.py
@@ -1,3 +1,5 @@
+# Using While loop:
+# ----------------
 start = 1
 end = int(input("Enter your Numb: "))
 
@@ -5,3 +7,13 @@ while end >= 1 :
 	start = start * end
 	end -= 1
 print(start)
+
+
+# Using For loop:
+# ---------------
+num = int(input("Enter the number? "))
+total = 1
+
+for x in range(1, num + 1):
+    total = total * x
+print(total)
