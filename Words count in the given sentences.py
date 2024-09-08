30288cb8983536267edca1a4d396363f0a3ee3a3
Biruthivin B K <131096179+Brithiv@users.noreply.github.com>
2024-09-08 06:53:15 +0530

    Words count in the given sentences.py

--- Words count in the given sentences.py
+++ Words count in the given sentences.py
@@ -1,3 +1,5 @@
+# Using while loop:
+# -----------------
 sent = input("Enter the Sentences: ")
 
 i = 0
@@ -9,3 +11,13 @@ while i < length :
 		count += 1
 	i += 1
 print("Count in the sentences are ", count)
+
+# Using for loop:
+# ---------------
+sent = "Welcome to python"       
+wordsCount = 1
+
+for x in sent :
+    if x==" " :
+        wordsCount+=1
+print(wordsCount)
