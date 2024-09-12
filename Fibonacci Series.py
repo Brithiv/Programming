76417938b2dae75340620a3fd8301435b3683dbe
Biruthivin B K <131096179+Brithiv@users.noreply.github.com>
2024-09-12 09:33:42 +0530

    Fibonacci Series.py

--- Fibonacci Series.py
+++ Fibonacci Series.py
@@ -1,3 +1,5 @@
+# Using while loop:
+# ----------------
 f,s = -1,1
 n = 10		
 
@@ -7,3 +9,16 @@ while 0<n :
     f=s
     s=t
     n-=1
+
+# Using for loop:
+# --------------
+f=-1
+s=1
+num = int(input("Enter the number? "))
+
+for i in range(num+1):
+    t=f+s
+    print(t, end=" ")
+    f=s
+    s=t
+    
